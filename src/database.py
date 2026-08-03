a57fd9e1b5684d159987f7d22f3ca6249ce926e8
mostafaeltaweel <mostafa.eltaweel000@gmail.com>
2026-08-03 19:11:13 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -264,7 +264,7 @@ def get_consumer_by_id(cons_no: str) -> Dict[str, Any]:
 def get_all_consumer_ids(limit: int = None) -> List[str]:   #limit: int = 500
     """Indexed SQL query (<20ms) to fetch customer ID list."""
     with get_db() as conn:
-        rows = conn.execute("SELECT CONS_NO FROM consumers LIMIT ?;"), (limit,).fetchall()  #
+        rows = conn.execute("SELECT CONS_NO FROM consumers LIMIT ?;"), (limit,)).fetchall()  #
     return [r["CONS_NO"] for r in rows]
 
 def get_recent_prediction_history(limit: int = 5) -> pd.DataFrame:
